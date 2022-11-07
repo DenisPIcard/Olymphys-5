d1b1e51cda31b0542e09ec4ebcc637e1ac251a86
AlainJouve <alain.jouve@wanadoo.fr>
2022-11-07 14:02:25 +0100

    Corrections diverses

--- src/Form/Type/Admin/CustomCentreFilterType.php
+++ src/Form/Type/Admin/CustomCentreFilterType.php
@@ -3,6 +3,7 @@
 namespace App\Form\Type\Admin;
 
 use App\Entity\Centrescia;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -14,23 +15,26 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class CustomCentreFilterType extends AbstractType
 {
     private $requestStack;
+    private EntityManagerInterface $doctrine;
 
-    public function __construct(RequestStack $requestStack)
+    public function __construct(RequestStack $requestStack, EntityManagerInterface $doctrine)
     {
         $this->requestStack = $requestStack;
-
+        $this->doctrine=$doctrine;
     }
 
 
     public function configureOptions(OptionsResolver $resolver)
 
     {
+        $listeCentres=$this->doctrine->getRepository(Centrescia::class)->findBy(['actif'=>true]);
+
         $resolver->setDefaults([
             'comparison_type_options' => ['type' => 'entity'],
             'value_type' => EntityType::class,
             'class' => Centrescia::class,
-            ''
-        ]);
+            'choices'=>$listeCentres
+            ]);
 
     }
 
