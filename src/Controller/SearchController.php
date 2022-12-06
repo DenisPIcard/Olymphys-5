43b8541b995040e217b20adc22fa883dbfd09027
AlainJouve <alain.jouve@wanadoo.fr>
2022-12-06 17:11:45 +0100

    Elastic search 1

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -10,24 +10,33 @@ use Elastica\Util;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends AbstractController
-{   private $finder;
-
-    public function __construct(PaginatedFinderInterface $finder)
-    {
-        $this->finder = $finder;
-    }
+{
     /**
-     * @param TransformedFinder $fichierspassesFinder
+     *
      * @return Response
      * @Route ("/search/searchAction", name="search")
      */
-    public function searchAction() : Response
+    public function searchAction(TransformedFinder $odpfFichierspassesFinder) : Response
     {
-        $results = $this->finder->find('example.net');
-
+        //$finder = $this->container->get('fos_elastica.finder.odpfFichierspasses');
+        //$results = $this->finder->find('');
+       $search = Util::escapeTerm('30-eq-5-Resume-Les Cafeines.pdf');
+        //dd($odpfFichierspassesFinder);
+        $params = [
+            'index' => 'odpfFichierspasses',
+            'body'  => [
+                'query' => [
+                    'match' => [
+                        'testField' => $search
+                    ]
+                ]
+            ]
+        ];
+      $results =  $odpfFichierspassesFinder->find($search, 10);
+        //dump($search);
         // Option 2. Returns a set of hybrid results that contain all Elasticsearch results
         // and their transformed counterparts. Each result is an instance of a HybridResult
-        $results = $this->finder->findHybrid('soleil');
+
         dd($results);
 
         return $this->render("views/empty.html.twig");
