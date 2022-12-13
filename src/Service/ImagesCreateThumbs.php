210626efd1335dad2444b1e5f3ce2d91782a662f
AlainJouve <alain.jouve@wanadoo.fr>
2022-12-13 14:07:47 +0100

    Possibilité de charger différents types de d'image. Conversion automatique en jpg

--- src/Service/ImagesCreateThumbs.php
+++ src/Service/ImagesCreateThumbs.php
@@ -6,11 +6,14 @@ namespace App\Service;
 use App\Entity\Odpf\OdpfImagescarousels;
 use App\Entity\Odpf\OdpfLogos;
 use App\Entity\Photos;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp;
 
 use Exception;
 use Imagick;
 use ImagickException;
+use PhpParser\Node\Scalar\String_;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 
 class ImagesCreateThumbs
@@ -101,7 +104,7 @@ class ImagesCreateThumbs
                 $fond->writeImage($fileImage);
             } catch (\Exception $e) {
 
-                dd($e);
+
             }
         }
 
@@ -109,57 +112,55 @@ class ImagesCreateThumbs
         if ($image instanceof Photos) {
             $path = 'odpf/odpf-archives/' . $image->getEditionspassees()->getEdition() . '/photoseq/';
             $pathThumb = $path . 'thumbs/';
-            if (file_exists($path . $image->getPhoto())){
-            $fileImage = $image->getPhotoFile();
-            $imageOrig = new Imagick($fileImage);
-            $imageOrig->readImage($path . $image->getPhoto());
-            $properties = $imageOrig->getImageProperties();
-            $heightOrig = $imageOrig->getImageHeight();
-            $widthOrig = $imageOrig->getImageWidth();
-            if (isset($properties['exif:Orientation'])) {
+            $nomPhoto= $image->getPhoto();
+            if (file_exists($path .$nomPhoto)){
+                $fileImage = $image->getPhotoFile();
+                $imageOrig = new Imagick($fileImage);
+                $imageOrig->readImage($path . $image->getPhoto());
+                $properties = $imageOrig->getImageProperties();
+                $heightOrig = $imageOrig->getImageHeight();
+                $widthOrig = $imageOrig->getImageWidth();
+                if (isset($properties['exif:Orientation'])) {
 
-                if ($properties['exif:Orientation'] == 8) {//(270°)
-                    $heightOrig = $imageOrig->getImageWidth();
-                    $widthOrig = $imageOrig->getImageHeight();
-                    $imageOrig->rotateImage('#000', -90);
+                    if ($properties['exif:Orientation'] == 8) {//(270°)
+                        $heightOrig = $imageOrig->getImageWidth();
+                        $widthOrig = $imageOrig->getImageHeight();
+                        $imageOrig->rotateImage('#000', -90);
+                    }
+                    if ($properties['exif:Orientation'] == 6) {
+                        $heightOrig = $imageOrig->getImageWidth();
+                        $widthOrig = $imageOrig->getImageHeight();
+                        $imageOrig->rotateImage('#000', 90);
+                    }
                 }
-                if ($properties['exif:Orientation'] == 6) {
-                    $heightOrig = $imageOrig->getImageWidth();
-                    $widthOrig = $imageOrig->getImageHeight();
-                    $imageOrig->rotateImage('#000', 90);
-                }
-            }
-            $percent = 200 / $heightOrig;
-            $nllwidth = $widthOrig * $percent;
-            $nllheight = 200;
-            $fond = new Imagick('images/fond_noir_carousel.jpg');
-            $formatCouleur = $imageOrig->getImageColorspace();
-            $y = (200 - $nllheight) / 2;
-
-            $x = (230 - $nllwidth) / 2;
-            if (($formatCouleur == imagick::COLORSPACE_CMYK) or ($formatCouleur == imagick::COLORSPACE_CMY)) {
-                $imageOrig->transformImageColorspace(imagick::COLORSPACE_RGB);
-            }
-            if ($widthOrig * $percent <= 230) {
-                $imageOrig->resizeImage($nllwidth, $nllheight, imagick::FILTER_LANCZOS, 1);
-                $fond->compositeImage($imageOrig, imagick::COMPOSITE_OVER, $x, $y);
-            }
-
-            if ($widthOrig * $percent > 230) {
-                $nllwidth = 230;
-                $nllheight = $heightOrig * 230 / $widthOrig;
+                $percent = 200 / $heightOrig;
+                $nllwidth = $widthOrig * $percent;
+                $nllheight = 200;
+                $fond = new Imagick('images/fond_noir_carousel.jpg');
+                $formatCouleur = $imageOrig->getImageColorspace();
                 $y = (200 - $nllheight) / 2;
+
                 $x = (230 - $nllwidth) / 2;
-                $imageOrig->resizeImage($nllwidth, $nllheight, imagick::FILTER_LANCZOS, 1);
-            }
-            $fond->compositeImage($imageOrig, imagick::COMPOSITE_OVER, $x, $y);
-            $fond->setColorspace(imagick::COLORSPACE_RGB);
-            $fond->writeImage($pathThumb . $image->getPhoto());
+                if (($formatCouleur == imagick::COLORSPACE_CMYK) or ($formatCouleur == imagick::COLORSPACE_CMY)) {
+                    $imageOrig->transformImageColorspace(imagick::COLORSPACE_RGB);
+                }
+                if ($widthOrig * $percent <= 230) {
+                    $imageOrig->resizeImage($nllwidth, $nllheight, imagick::FILTER_LANCZOS, 1);
+                    $fond->compositeImage($imageOrig, imagick::COMPOSITE_OVER, $x, $y);
+                }
+
+                if ($widthOrig * $percent > 230) {
+                    $nllwidth = 230;
+                    $nllheight = $heightOrig * 230 / $widthOrig;
+                    $y = (200 - $nllheight) / 2;
+                    $x = (230 - $nllwidth) / 2;
+                    $imageOrig->resizeImage($nllwidth, $nllheight, imagick::FILTER_LANCZOS, 1);
+                }
+                $fond->compositeImage($imageOrig, imagick::COMPOSITE_OVER, $x, $y);
+                $fond->setColorspace(imagick::COLORSPACE_RGB);
+                $fond->writeImage($pathThumb . $image->getPhoto());
+                }
             }
         }
-
-
-    }
-
 
 }
