753828a3cb61ba17fa2ae7c65339167bf3fac70a
AlainJouve <alain.jouve@wanadoo.fr>
2023-01-09 21:07:01 +0100

    mise au point elastica search(infructeuse)

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Odpf\OdpfFichierspasses;
+use FOS\ElasticaBundle\Configuration\ManagerInterface;
 use FOS\ElasticaBundle\Finder\PaginatedFinderInterface;
 use FOS\ElasticaBundle\Manager\RepositoryManagerInterface;
+use Smalot\PdfParser\Parser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -12,13 +14,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends AbstractController
 {
-    private RepositoryManagerInterface $repositoryManager;
+    private ManagerInterface $doctrine;
     private PaginatedFinderInterface $finder;
 
-    public function __construct(PaginatedFinderInterface $finder, RepositoryManagerInterface $repositoryManager)
+    public function __construct(PaginatedFinderInterface $finder, ManagerInterface $doctrine)
     {
         $this->finder = $finder;
-        $this->repositoryManager = $repositoryManager;
+        $this->doctrine = $doctrine;
     }
     /**
      *
@@ -27,32 +29,10 @@ class SearchController extends AbstractController
      */
     public function searchAction()
     {
-        //$finder = $this->container->get('fos_elastica.finder.odpfFichierspasses');
-       //$results = $this->finder->find('Soleil');
-       //$search = Util::escapeTerm('30-eq-5-Resume-Les Cafeines.pdf');
-        //dd($odpfFichierspassesFinder);
 
-
-        //dump($search);
-        // Option 2. Returns a set of hybrid results that contain all Elasticsearch results
-        // and their transformed counterparts. Each result is an instance of a HybridResult
-       /* $paginator = $this->get('knp_paginator');
-        $results = $this->finder->createPaginatorAdapter('bob');
-        $pagination = $paginator->paginate($results, $page, 10);
-
-        $options = [
-            'sortNestedPath' => 'owner',
-            'sortNestedFilter' => new Query\Term(['enabled' => ['value' => true]]),
-        ];*/
-        $repository = $this->repositoryManager->getRepository(OdpfFichierspasses::class);
-
-
-        $articles = $repository->find('soleil');
+        $articles = $this->finder->findHybrid('soleil');
         dd($articles);
-        // sortNestedPath and sortNestedFilter also accepts a callable
-        // which takes the current sort field to get the correct sort path/filter
 
-       // $pagination = $paginator->paginate($results, $page, 10, $options);
     }
 
 }
