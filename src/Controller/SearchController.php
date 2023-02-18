dc305ea9e4ac2d96a7f2c6e23b475e47654cd6b6
AlainJouve <alain.jouve@wanadoo.fr>
2023-02-18 16:30:55 +0100

    reprise d'elasticasearch

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Odpf\OdpfFichierspasses;
-use FOS\ElasticaBundle\Configuration\ManagerInterface;
+
+
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\ElasticaBundle\Finder\PaginatedFinderInterface;
 use FOS\ElasticaBundle\Manager\RepositoryManagerInterface;
 use Smalot\PdfParser\Parser;
@@ -14,19 +16,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends AbstractController
 {
-    private ManagerInterface $doctrine;
-    private PaginatedFinderInterface $finder;
 
-    public function __construct(PaginatedFinderInterface $finder, ManagerInterface $doctrine)
+
+    public function __construct(private PaginatedFinderInterface $finder, EntityManagerInterface $doctrine)
     {
-        $this->finder = $finder;
-        $this->doctrine = $doctrine;
+
     }
-    /**
-     *
-     * @return Response
-     * @Route ("/search/searchAction", name="search")
-     */
+    #[Route("/search/searchAction", name:"search")]
+
     public function searchAction()
     {
 
