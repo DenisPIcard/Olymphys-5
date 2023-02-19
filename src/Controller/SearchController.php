3cc6a58b01abe2fde213ba7b164ff4b89cbf3ba5
AlainJouve <alain.jouve@wanadoo.fr>
2023-02-19 11:35:54 +0100

    reprise d'elasticasearch echec

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -6,29 +6,31 @@ use App\Entity\Odpf\OdpfFichierspasses;
 
 
 use Doctrine\ORM\EntityManagerInterface;
+use Elastica\Aggregation\Terms;
+use Elastica\Query;
+use Elastica\Util;
 use FOS\ElasticaBundle\Finder\PaginatedFinderInterface;
+use FOS\ElasticaBundle\Finder\TransformedFinder;
 use FOS\ElasticaBundle\Manager\RepositoryManagerInterface;
 use Smalot\PdfParser\Parser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends AbstractController
 {
-
-
-    public function __construct(private PaginatedFinderInterface $finder, EntityManagerInterface $doctrine)
+    public function __construct(private readonly PaginatedFinderInterface $finder)
     {
 
     }
     #[Route("/search/searchAction", name:"search")]
-
-    public function searchAction()
+    public function searchAction(): Response
     {
 
-        $articles = $this->finder->findHybrid('soleil');
-        dd($articles);
+        $results = $this->finder->find('memoire');
+        dd($results);
 
     }
 
